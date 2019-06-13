5a2ec4f434b2f5b3a12ed80e754fe113be4567bb
HasibKUCSE <hasibsdvrt555@gmail.com>
2019-06-13 13:51:41 +0600

    api view

--- api/views.py
+++ api/views.py
@@ -1,4 +1,20 @@
-from django.http.response import JsonResponse
+# from django.http.response import JsonResponse
+#
+# from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+
+# class HelloWorldView(APIView):
+#     def get(self, request):
+#         return Response({"message": "Hello World"})
+
+@api_view(["GET", "POST"])
 def hello_world(request):
-    return JsonResponse({"message": "hello world"})
+    if request.method == "GET":
+        return Response({"message": "Hello World"})
+    else:
+        name = request.data.get("name")
+        if not name:
+            return Response({"error":"No name"})
+        return  Response({"message": "Hello {}!".format(name)})
